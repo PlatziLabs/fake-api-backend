be3c96fd5b1042a0a19a4b6190cc7fe859b7a914
Nicolas Molina Monroy <hi@nicobytes.com>
2021-07-26 13:06:06 -0400

    :construction: work
    work

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+  app.enableCors();
 
   app.useGlobalPipes(
     new ValidationPipe({
