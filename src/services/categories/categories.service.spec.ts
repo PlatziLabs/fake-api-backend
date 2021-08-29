5536e9f7597414fbf951b55dd504525d302ca327
Erik R Sánchez Pérez <erik@yalochat.com>
2021-08-29 18:53:14 -0500

    test: added categories.service tests

--- src/services/categories/categories.service.spec.ts
+++ src/services/categories/categories.service.spec.ts
@@ -1,7 +1,35 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CategoriesService } from './categories.service';
 
-describe('CategoriesService', () => {
+const fictureCategories = [
+  {
+    id: 1,
+    name: 'Clothes',
+    typeImg: 'people',
+  },
+  {
+    id: 2,
+    name: 'Electronics',
+    typeImg: 'tech',
+  },
+  {
+    id: 3,
+    name: 'Furniture',
+    typeImg: 'arch',
+  },
+  {
+    id: 4,
+    name: 'Toys',
+    typeImg: 'any',
+  },
+  {
+    id: 5,
+    name: 'Others',
+    typeImg: 'animals',
+  },
+];
+
+describe(`Inspect ${CategoriesService.name} class`, () => {
   let service: CategoriesService;
 
   beforeEach(async () => {
@@ -12,7 +40,26 @@ describe('CategoriesService', () => {
     service = module.get<CategoriesService>(CategoriesService);
   });
 
-  it('should be defined', () => {
+  it(`created ${CategoriesService.name} should be defined`, () => {
     expect(service).toBeDefined();
+  });
+  it(`call ${CategoriesService.name}.getAll() should be successfully`, () => {
+    const actual = service.getAll();
+    expect(actual).toStrictEqual(fictureCategories);
+  });
+  it(`call ${CategoriesService.name}.getCategory() should be successfully`, () => {
+    const actual = service.getCategory(3);
+    expect(actual).toStrictEqual(fictureCategories[2]);
+  });
+  it(`call ${CategoriesService.name}.getCategory() should be unsuccessfully`, () => {
+    const actual = service.getCategory(-1);
+    expect(actual).toBeNull();
+  });
+  it(`call ${CategoriesService.name}.create() should be successfully`, () => {
+    const data = { name: 'Hello World', typeImg: 'TheWorld' };
+    const actual = service.create(data);
+    expect(actual).toStrictEqual(
+      Object.assign({}, data, { id: fictureCategories.length + 1 }),
+    );
   });
 });
