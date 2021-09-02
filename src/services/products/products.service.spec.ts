52f35ef6f7c826243425e88e9072e0869ed7c9c3
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2021-09-01 22:33:27 -0500

    add test to service get all

--- src/services/products/products.service.spec.ts
+++ src/services/products/products.service.spec.ts
@@ -1,12 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ProductsService } from './products.service';
+import { CategoriesService } from '../categories/categories.service';
+import { FilterProductsDto } from '../../dto/product.dto';
 
 describe('ProductsService', () => {
   let service: ProductsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ProductsService],
+      providers: [ProductsService, CategoriesService],
     }).compile();
 
     service = module.get<ProductsService>(ProductsService);
@@ -14,5 +16,20 @@ describe('ProductsService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('Test the service getAll', () => {
+    it('should return all the products', () => {
+      const filters = new FilterProductsDto();
+      const productsAll = service.getAll(filters);
+
+      const newFilters = new FilterProductsDto();
+      const productsFiltered = service.getAll(newFilters);
+
+      expect(productsFiltered).toStrictEqual(productsAll);
+
+      const products = service.getAll(newFilters);
+      expect(products).toStrictEqual(productsAll);
+    });
   });
 });
