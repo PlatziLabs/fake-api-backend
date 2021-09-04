bee775321bd16c03270c3e7fc35af809fabd7b2b
Erik Sanchez <erik.sape@gmail.com>
2021-09-04 08:21:28 -0500

    tests: improve library tests descriptions

--- libs/data-set/src/data-set.service.spec.ts
+++ libs/data-set/src/data-set.service.spec.ts
@@ -20,33 +20,40 @@ describe('DataSetService', () => {
     expect(service).toBeDefined();
   });
 
-  it('should return data', () => {
-    const fixture = { id: 1, name: 'new category' };
-    service.fill([fixture]);
+  it('should return all data', () => {
+    const fixture = [
+      { id: 1, name: 'new category' },
+      { id: 2, name: 'new cat' },
+      { id: 3, name: 'new' },
+    ];
+    service.fill(fixture);
     const expected = service.get();
-    expect(expected).toStrictEqual([fixture]);
-  });
-
-  it('should return data first', () => {
-    const fixture = { id: 1, name: 'new category' };
-    service.fill([fixture]);
-    const expected = service.first();
     expect(expected).toStrictEqual(fixture);
   });
 
-  it('should return null in first method', () => {
+  it('should return first element', () => {
+    const fixture = [
+      { id: 1, name: 'new category' },
+      { id: 2, name: 'new cat' },
+    ];
+    service.fill(fixture);
+    const expected = service.first();
+    expect(expected).toStrictEqual(fixture[0]);
+  });
+
+  it('should return null getting first element', () => {
     const expected = service.first();
     expect(expected).toBeNull();
   });
 
-  it('should return created data', () => {
+  it('create dataset should return created data', () => {
     const name = 'new Category 2';
     const fixture = { id: 1, name };
     const expected = service.create({ name });
     expect(expected).toStrictEqual(fixture);
   });
 
-  it('should filter and then get first', () => {
+  it('when filter should get first match', () => {
     const fixture = [
       { id: 1, name: 'new category' },
       { id: 2, name: 'new cat' },
