7e4141fb019b234df388e851106bc30788da262b
Erik Sanchez <erik.sape@gmail.com>
2021-09-08 07:19:21 -0500

    chore: updated eslintrc.js to allow Function type

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,5 +20,13 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    "@typescript-eslint/ban-types": ["error",
+      {
+        "types": {
+          "Function": false,
+        },
+        "extendDefaults": true
+      }
+    ]
   },
 };
