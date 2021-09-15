fdcbbe40b48e4b8d8787701c3c401ff1aa11c1d6
Erik Sanchez <erik.sape@gmail.com>
2021-09-15 08:11:13 -0500

    fix: removed unused variable

--- src/dto/category.dto.ts
+++ src/dto/category.dto.ts
@@ -1,4 +1,4 @@
-import { IsString, IsNotEmpty, isDefined } from 'class-validator';
+import { IsString, IsNotEmpty } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateCategorytDto {
