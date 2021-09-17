8484ef9f67ef93701bff4f7eba0f5c623d904eb5
Erik Sanchez <erik.sape@gmail.com>
2021-09-16 20:40:16 -0500

    chore: remove unused lines

--- src/controllers/auth/auth.controller.ts
+++ src/controllers/auth/auth.controller.ts
@@ -1,11 +1,9 @@
 import { Controller, Post, UseGuards, Req, Get } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { AuthGuard } from '@nestjs/passport';
-import { Request } from 'express';
 
 import { AuthService } from '../../services/auth/auth.service';
 import { UsersService } from '../../services/users/users.service';
-import { User } from '../../models/user.model';
 
 @ApiTags('auth')
 @Controller('auth')
@@ -17,16 +15,15 @@ export class AuthController {
 
   @UseGuards(AuthGuard('local'))
   @Post('login')
-  login(@Req() req: Request) {
+  login(@Req() req) {
     return this.authService.generateJWT(req.user);
   }
 
   @UseGuards(AuthGuard('jwt'))
   @Get('profile')
-  profile(@Req() req: any) {
+  profile(@Req() req) {
     const user = req.user;
     console.log('token', user);
     return this.usersService.getUser(user?.userId);
-    // return this.usersService.getUser(user?.id);
   }
 }
