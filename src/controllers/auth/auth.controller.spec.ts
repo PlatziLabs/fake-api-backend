9df1ab365f822fbc2b00d59d87d5d5031ec19901
Erik Sanchez <erik.sape@gmail.com>
2021-09-16 20:39:32 -0500

    tests: added test AuthController

--- src/controllers/auth/auth.controller.spec.ts
+++ src/controllers/auth/auth.controller.spec.ts
@@ -1,18 +1,79 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
+import { AuthService } from '../../services/auth/auth.service';
+import { UsersService } from '../../services/users/users.service';
+import { JwtModule } from '@nestjs/jwt';
+import { Role } from '../../models/user.model';
 
 describe('AuthController', () => {
   let controller: AuthController;
+  let authService: AuthService;
+  let usersService: UsersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
+      providers: [AuthService, UsersService],
+      imports: [
+        JwtModule.register({
+          secret: 'my-cat',
+          signOptions: { expiresIn: '1h' },
+        }),
+      ],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
+    authService = module.get<AuthService>(AuthService);
+    usersService = module.get<UsersService>(UsersService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should login a user', () => {
+    const user = { email: 'mock@email.com', id: 1 };
+    const request = { user };
+    const returnValue = { access_token: 'mockJsonWebToken' };
+    const mock = jest
+      .spyOn(authService, 'generateJWT')
+      .mockReturnValueOnce(returnValue);
+    const mockInput = [user];
+
+    const expected = controller.login(request);
+
+    expect(expected).toStrictEqual(returnValue);
+    expect(mock).toHaveBeenCalledWith(...mockInput);
+  });
+
+  it('should return the profile', () => {
+    const user = {
+      id: 1,
+      email: 'mock@mail.com',
+      password: 'thePassword',
+      name: 'theName',
+      role: Role.customer,
+    };
+    const request = { user: { userId: user.id } };
+    const mock = jest.spyOn(usersService, 'getUser').mockReturnValueOnce(user);
+    const mockInput = [user.id];
+
+    const expected = controller.profile(request);
+
+    expect(expected).toStrictEqual(user);
+    expect(mock).toHaveBeenCalledWith(...mockInput);
+  });
+
+  it('should not return the profile', () => {
+    const request = {};
+    const mock = jest
+      .spyOn(usersService, 'getUser')
+      .mockReturnValueOnce(undefined);
+    const mockInput = [undefined];
+
+    const expected = controller.profile(request);
+
+    expect(expected).toStrictEqual(undefined);
+    expect(mock).toHaveBeenCalledWith(...mockInput);
   });
 });
