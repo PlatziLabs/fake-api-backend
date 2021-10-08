5f61b5f39838e1e7b17805fba30b7c8efd215078
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2021-10-07 19:02:25 -0500

    fix typo CreateCategoryDto

--- src/services/categories/categories.service.spec.ts
+++ src/services/categories/categories.service.spec.ts
@@ -34,7 +34,7 @@ describe(`Inspect ${CategoriesService.name} class`, () => {
   it(`get a category by id`, () => {
     const fixture = { id: 10, name: 'name', typeImg: 'typeImg' };
     const mock = jest.spyOn(dataSet, 'find').mockImplementationOnce(() => {
-      const dssFixture = new DataSetService<CreateCategorytDto>();
+      const dssFixture = new DataSetService<CreateCategoryDto>();
       dssFixture.fill([fixture], 10);
       return dssFixture;
     });
