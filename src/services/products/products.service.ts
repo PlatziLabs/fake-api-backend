59b2bf6f12f28f32e34628b3491d7cd8e88eee81
Nicolas Molina Monroy <hi@nicobytes.com>
2022-05-18 20:37:23 -0400

    :construction: work

--- src/services/products/products.service.ts
+++ src/services/products/products.service.ts
@@ -87,7 +87,7 @@ export class ProductsService {
     }
     const { limit, offset } = params;
     if (limit > 0 && offset >= 0) {
-      const end = offset + limit * 3;
+      const end = offset + limit;
       productsWithParams = productsWithParams.slice(offset, end);
     }
     return productsWithParams;
