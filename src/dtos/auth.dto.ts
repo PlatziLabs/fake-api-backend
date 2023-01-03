4c12ef528b36a0a56cd87a9a37ce64b942efc98f
Nicolas Molina Monroy <hi@nicobytes.com>
2023-01-03 13:18:51 -0400

    :bug: Fix refresh token

--- src/dtos/auth.dto.ts
+++ src/dtos/auth.dto.ts
@@ -1,10 +1,9 @@
-import { IsAlphanumeric, IsNotEmpty, IsString } from 'class-validator';
+import { IsNotEmpty, IsString } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class RefreshTokenDto {
   @ApiProperty()
   @IsString()
-  @IsAlphanumeric()
   @IsNotEmpty()
   refreshToken: string;
 }
